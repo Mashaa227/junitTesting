9abaac939fe11bb8c5ca4290d7634da7c8c2bc67
mashaa227 <mmba6065@gmail.com>
2025-05-27 13:25:14 +0530

    Using Response matchers to check status and content

--- src/test/java/com/vwits/junit_testing/restcontroller/HelloWordController.java
+++ src/test/java/com/vwits/junit_testing/restcontroller/HelloWordController.java
@@ -6,11 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(HelloWordController.class)
  class HelloWordControllerTest {
@@ -23,10 +23,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
     void helloWord() throws Exception {
         //call helloWord
         RequestBuilder request = MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON);
-        MvcResult result = mockMvc.perform(request).andReturn();
+                 mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello I am Masha"))
+                .andReturn();
 
         //verify Hello Word
-        assertEquals("Hello I am Masha", result.getResponse().getContentAsString());
+
     }
 
 }
